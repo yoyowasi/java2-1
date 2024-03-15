7b00d62e9d5105f1dc1d0de433855cf03319f55f
yoyowasi <162660522+yoyowasi@users.noreply.github.com>
2024-03-15 22:30:33 +0900

    3월 15일 강의내용 정리

--- hello.java
+++ hello.java
@@ -1,10 +1,6 @@
 public class hello {
-
 	public static void main(String[] args) {
-		System.out.println("Hello vscode가 좋아요");
-		
-
+		System.out.println("Hello!!! vscode가 좋아요");
 	}
-
 }
-***Test
+///Test
