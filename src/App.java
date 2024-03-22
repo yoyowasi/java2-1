dc65eb293f7eaa12b3ed75cacac935e4c21362ee
yoyowasi <youmin302@gmail.com>
2024-03-22 12:48:32 +0900

    12312

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var foo = 200;
+        var name = "minhyeok";
+        
+        System.out.println(foo + name);
+        
     }
 }
