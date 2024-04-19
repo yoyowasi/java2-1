53b134fa10f59ed820da036f163aeb4bf08a5da3
yoyowasi <youmin302@gmail.com>
2024-04-19 12:14:14 +0900

    4월 19일 수업종료

---
+++ src/chap05/Ex05_02.java
@@ -0,0 +1,14 @@
+package chap05;
+
+class Colorpoint extends Point {
+    public void showColorpoint(){
+        
+    }
+}
+
+public class Ex05_02{
+    public static void main(String[] args) {
+        ColorPoint cp = new ColorPoint();
+        cp.showColorPoint();
+    }
+}
